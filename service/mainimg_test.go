de98c172f805e4dc7b1e89aece7bf6fc0b8f4b3a
maratrix <maratrix@163.com>
2018-12-17 17:00:27 +0800

    添加测试用例

---
+++ service/mainimg_test.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"image/color"
+	"testing"
+
+	"github.com/Unknwon/com"
+)
+
+func TestMainImg_Do(t *testing.T) {
+	tmpDir, _ := com.GetSrcPath("emojigo/public")
+	m := &MainImg{
+		TextSlice: []string{
+			"以梦为马，越骑越傻",
+			"诗和远方，越远越脏",
+			"执子之手，如同猪肘",
+			"故事和酒，淘宝都有",
+			"春风十里，吹不死你",
+			"心有猛虎，像二百五",
+			"嘘寒问暖，不如巨款",
+			"闲庭信步，忘穿秋裤",
+			"面朝大海，笑出精彩",
+			"白云苍狗，你比我丑",
+			"岁月静好，全靠胆小",
+			"你若安好，打支付宝",
+			"寒风十里，冷的飞起",
+			"没有梦想，过得特爽",
+			"不愿将就，装逼没够",
+			"随遇而安，得脑血栓",
+		},
+		Path:         tmpDir,
+		FontSize:     200,
+		IconTitle:    "搞笑，文字",
+		IconColor:    color.Transparent,
+		ProfileTitle: "岁月静好全靠胆小",
+		ProfileColor: color.NRGBA{239, 233, 87, 255},
+		Theme:        "",
+	}
+
+	err := m.Do()
+
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
